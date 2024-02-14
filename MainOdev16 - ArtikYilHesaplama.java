c85cb7b26a1e8b7e226a64e4f3023183da4b61d2
936825714456 <90316441+936825714456@users.noreply.github.com>
2024-02-14 21:06:21 +0100

    Add files via upload

---
+++ MainOdev16 - ArtikYilHesaplama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int yil;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz : ");
+        yil = input.nextInt();
+
+        if((yil % 4 == 0 && yil % 100 !=0) || (yil % 400 == 0)){
+            System.out.print(yil + " Bir artık yıldır.");
+        }else{
+            System.out.print(yil + " Bir artık yıl değildir.");
+        }
+
+
+
+    }
+}
